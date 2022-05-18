d1c911a976af93f601128f3dd2363ac6fec4f874
Remi <remi.humbert007@gmail.com>
2022-05-18 11:21:26 +0200

    fixing the accordion

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
   });
+  coll[i].addEventListener("focusout", function() {
+    this.classList.remove("active");
+  });
 }
 
 $(document).ready(function() {
